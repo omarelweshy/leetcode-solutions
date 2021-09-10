b4000a8976bf33273e1cf9d3835a010dd3e8b415
Omar Elweshy <omarelweshy@gmail.com>
2021-09-10 17:06:01 +0200

    Create 42-Trapping-Rain-Water.js

---
+++ 42-Trapping-Rain-Water.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/trapping-rain-water
+
+var trap = function(height) {
+    let left = 0, right = height.length - 1, maxLeft = 0, maxRight = 0, total = 0
+
+	while (left < right) {
+		if (height[left] <= height[right]) {
+			if (height[left] >= maxLeft) {
+				maxLeft = height[left]
+			} else {
+				total += maxLeft - height[left]
+			}
+			left++
+		} else {
+			if (height[right] >= maxRight) {
+				maxRight = height[right]
+			} else {
+				total += maxRight - height[right]
+			}
+			right--
+		}
+	} 
+	return total
+};
